1236459736fd85c10ef953a55b8507253b3937e2
paridhomane <59290613+paridhomane@users.noreply.github.com>
2021-09-05 15:13:52 +0530

    first commit

---
+++ server/login.js
@@ -0,0 +1,60 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+app.use(express.static('../client'));
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodejs-login",
+});
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	// response.sendFile(path.join('../client/Screens/Login.html'));
+
+    response.sendFile(path.join(__dirname, '../client/Screens', 'Login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var email = request.body.email;
+	var password = request.body.password;
+	if (email && password) {
+		connection.query('SELECT * FROM users WHERE Email = ? AND Password = ?', [email, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.email = email;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.email + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000);
